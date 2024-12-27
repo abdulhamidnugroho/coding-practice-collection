c32b652bf2a88658a998850f6eef8c5c10e7c4d2
abdulhamidnugroho <abdulhamidnugroho@gmail.com>
2024-12-27 07:22:21 +0700

    contains duplicates

---
+++ array/contains-duplicates/contains-duplicate.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func containsDuplicate(nums []int) bool {
+	seen := make(map[int]bool)
+	for _, num := range nums {
+		if seen[num] {
+			return true
+		}
+		seen[num] = true
+	}
+	return false
+}
+
+func main() {
+	nums1 := []int{1, 2, 3, 1}
+	nums2 := []int{1, 2, 3, 4}
+	nums3 := []int{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}
+
+	fmt.Println(containsDuplicate(nums1)) // Output: true
+	fmt.Println(containsDuplicate(nums2)) // Output: false
+	fmt.Println(containsDuplicate(nums3)) // Output: true
+}
